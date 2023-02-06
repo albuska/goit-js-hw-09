c4c24f04f8b0f0ca60b7364f2f79bb3b8dbe6ff1
albuska <109683293+albuska@users.noreply.github.com>
2023-02-06 22:10:04 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector('.form');
+
+formRef.addEventListener('submit', onFormSubmit); 
+
+function onFormSubmit(event) {
+event.preventDefault();
+
+let delay = +formRef.delay.value; 
+const step = +formRef.step.value;
+const amount = +formRef.amount.value;
+
+for (let i = 1; i <= amount; i+=1) {
+  createPromise(i, delay).then(onResolve).catch(onReject); 
+  delay += step;   
+}
+
+event.currentTarget.reset(); 
+}
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+const shouldResolve = Math.random() > 0.3;
+
+setTimeout(() => {
   if (shouldResolve) {
-    // Fulfill
+resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+reject(`❌ Rejected promise ${position} in ${delay}ms`); 
   }
+}, delay)
+  }); 
+}
+
+function onResolve(value) {
+  Notiflix.Notify.success(value);
+}
+
+function onReject(error) {
+  Notiflix.Notify.failure(error);
 }
